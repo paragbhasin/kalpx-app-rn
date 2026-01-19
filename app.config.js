06ca4fd6fc69035b81012c60e3ebfd58e376b054
Nayak Pavani <nayakpavani676@gmail.com>
2026-01-19 16:55:14 +0530

    version

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ module.exports = {
   expo: {
     name: "kalpx",
     slug: "kalpx",
-    version: "1.1.18",
+    version: "1.1.19",
     orientation: "portrait",
     icon: "./assets/AppIconImg.png",
     scheme: "kalpx",

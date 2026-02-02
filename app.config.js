581619b0f45df804319dc79c724ac16fecf74f80
Nayak Pavani <nayakpavani676@gmail.com>
2026-02-02 08:23:52 +0530

    build number updated

--- app.config.js
+++ app.config.js
@@ -57,7 +57,7 @@ module.exports = {
       },
     },
     android: {
-      versionCode: 26,
+      versionCode: 27,
       package: "com.kalpx.app",
       adaptiveIcon: {
         foregroundImage: "./assets/AppIconImg.png",
